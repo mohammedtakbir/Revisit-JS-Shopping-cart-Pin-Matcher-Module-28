df7d02f1516598f138d69bd9081941ae8e4f0ef4
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-21 19:13:01 +0600

    how typed number digits on the input filled

--- Pin-Matcher/js/pin-matcher.js
+++ Pin-Matcher/js/pin-matcher.js
@@ -16,3 +16,24 @@ document.getElementById('generate-pin').addEventListener('click', () => {
     const displayPin = document.getElementById('display-pin');
     displayPin.value = pin;
 })
+
+document.getElementById('calculator').addEventListener('click', (e) => {
+    const number = e.target.innerText;
+    const typedPin = document.getElementById('typed-pin');
+    if (isNaN(number)) {
+        if (number === 'C') {
+            typedPin.value = '';
+        } else if (number === '<') {
+            const digits = typedPin.value.split('');
+            digits.pop();
+            const remain = digits.join('');
+            typedPin.value = remain;
+
+            // typedPin.value.slice(typedPin.value.length - 1, typedPin.value.length);
+            // const remain = typedPin.value.slice(0, typedPin.value.length - 1);
+            // typedPin.value = remain;
+        }
+    } else {
+        typedPin.value += number
+    }
+})
