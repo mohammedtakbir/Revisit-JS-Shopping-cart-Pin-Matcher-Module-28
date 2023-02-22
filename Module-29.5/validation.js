8c04142933d4cf4132f200625558af6d8ff265db
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-22 18:18:14 +0600

    explore inNaN

---
+++ Module-29.5/validation.js
@@ -0,0 +1,11 @@
+const num = '0';
+// if (isNaN(num)) {
+//     console.log('Not a number');
+// } else {
+//     console.log('Number');
+// }
+if (typeof num === 'number') {
+    console.log('Number');
+} else {
+    console.log('Not a number');
+}
